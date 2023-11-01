41b34aa675ddd73e304006bac60651fa3da83b5b
rs_man <rs1192.work@gmail.com>
2023-11-01 15:48:09 +0200

    Working Version 1.0

---
+++ bot.py
@@ -0,0 +1,38 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from config_data.config import BOT_TOKEN
+from handlers import other_handlers, user_handlers
+
+# Инициализируем логгер
+logger = logging.getLogger(__name__)
+
+
+# Функция конфигурирования и запуска бота
+async def main():
+    # Конфигурируем логирование
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s')
+
+    # Выводим в консоль информацию о начале запуска бота
+    logger.info('Starting bot')
+
+    # Инициализируем бот и диспетчер
+    bot = Bot(token=BOT_TOKEN,
+              parse_mode='HTML')
+    dp = Dispatcher()
+
+    # Регистриуем роутеры в диспетчере
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
